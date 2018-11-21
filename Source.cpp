079f0a6b94c1ee2f54c50c44a70975826cf31d7c
Denis <lstelmah29@gmail.com>
2018-11-21 23:57:14 +0200

    zero lesson 0.3

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,7 @@
 #include "tgaimage.h"
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
+TGAColor RED(255, 0, 50, 40);
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, red);
-	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	image.write_tga_file("output.tga"); 
-		return 0;
+	TGAImage image(200,200,TGAImage::RGBA);
+	image.set(100, 50, RED);
+	image.write_tga_file("image.tga");
 }
